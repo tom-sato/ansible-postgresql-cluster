b2126e825ff478d1d4d19b91dc2bd49915963d77
Tomoaki Sato <sato@sraoss.co.jp>
2023-10-11 06:12:49 +0900

    Fix my_next_ip filter to return addresses up to x.x.x.254

--- filter_plugins/my_next_ip.py
+++ filter_plugins/my_next_ip.py
@@ -1,5 +1,5 @@
 def my_next_ip(items):
-    return '.'.join([list(items)[0].rsplit('.', 1)[0], str(min(set(range(3, 255)) - set(map(lambda x: x.split('.')[3], list(items)))))])
+    return '.'.join([list(items)[0].rsplit('.', 1)[0], str(min(set(range(3, 254)) - set(map(lambda x: x.split('.')[3], list(items)))))])
 class FilterModule(object):
     def filters(self):
         return {
